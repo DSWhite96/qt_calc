7189ea82a2be2b1325db6e5c416e8bf2df677169
Derrick White <derrickswhite@outlook.com>
2025-06-06 23:58:41 -0700

    Basic button functionality implemented

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+import operator
 import pathlib
 import sys
 
@@ -5,12 +6,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 
 class CalcGUI(QMainWindow):
-    """
-    Class representing the different elements of the GUI for this calculator application
-    """
     def __init__(self):
         """
-        Constructor
+        Class representing the different elements of the GUI for this calculator application
         """
         super(CalcGUI, self).__init__()
         path = str(pathlib.Path(__file__).parent.resolve()) + "/calc.ui"
@@ -29,6 +27,25 @@ class CalcGUI(QMainWindow):
         self.nineButton.clicked.connect(lambda: self.displayInput("9"))
         self.decimalButton.clicked.connect(lambda: self.displayInput("."))
 
+        #Defining input from operation buttons
+        self.addButton.clicked.connect(lambda: self.startOperation("+"))
+        self.subButton.clicked.connect(lambda: self.startOperation("-"))
+        self.multButton.clicked.connect(lambda: self.startOperation("*"))
+        self.divButton.clicked.connect(lambda: self.startOperation("/"))
+
+        #Running operation when equals button is clicked
+        self.equalButton.clicked.connect(lambda: self.performCalculation())
+
+        #Deleting most recent character when DEL button is clicked
+        self.delButton.clicked.connect(lambda: self.deleteChar())
+
+        #Clearing data when clear button is clicked
+        self.clearButton.clicked.connect(lambda: self.clearData())
+
+        #Tracking the current data and operation being used to perform a calculation
+        self.dataCache = ""
+        self.operation = ""
+
     def displayInput(self, val):
         """
         Reads input from calculator buttons and displays the value in the screen
@@ -40,12 +57,110 @@ class CalcGUI(QMainWindow):
         """
         #Get data that is currently in the calculator display
         currentData = self.outputLabel.text()
+        #If a decimal is being added to zero, start input as 0.
+        if currentData == "0" and val == ".":
+            self.outputLabel.setText("0.")
         #If the value is zero, simply display the new input
-        if currentData == "0":
-            self.outputLabel.setText(val)
+        elif currentData == "0":
+            self.outputLabel.setText(val) 
         else:
             #Otherwise, add the new input to the previously existing data
             self.outputLabel.setText(currentData + val)
+
+    def deleteChar(self):
+        """
+        Deletes the last entered character from the input string
+
+        Parameters:
+            None
+        Returns:
+            None
+        """
+        currentData = self.outputLabel.text()
+
+        #Delete the last entered character if the length of the input string is greater than one
+        if len(currentData) > 1:
+            self.outputLabel.setText(currentData[:-1])
+
+        #If only one character has been entered, reset the input string
+        elif currentData != "0":
+            self.outputLabel.setText("0")
+
+    def clearData(self):
+        """
+        Clears the cache, operation, and current data
+
+        Parameters:
+            None
+        Returns:
+            None
+        """
+        self.dataCache = ""
+        self.operation = ""
+        self.outputLabel.setText("0")
+
+    def startOperation(self, op):
+        """
+        Begins an opration to be performed on the inputed data
+
+        Parameters:
+            op (str) - Operation being performed for the current calculation
+        Returns:
+            None
+        """
+        #Read in data from calculator display
+        data = self.outputLabel.text()
+
+        #Try converting the data to a numerical value
+        try:
+            self.dataCache = float(data)
+
+            #If conversion is successful, update current operation
+            self.operation = op
+
+            #Reset input string
+            self.outputLabel.setText("0")
+
+        except ValueError:
+            #If that fails, reset values and return an error
+            self.clearData()
+            self.outputLabel.setText("ERROR")
+
+    def performCalculation(self):
+        """
+        Performs a calculation based on the currrent input and operation
+
+        Parameters:
+            None
+        Returns:
+            None
+        """
+        #Only perform calculation if an operation has begun
+        if self.dataCache and self.operation:
+            currentData = self.outputLabel.text()
+            try:
+                #Try to convert the data currently in the display to a numerical value
+                cdVal = float(currentData)
+
+                #Dict containing operations
+                ops = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv}
+
+                #Perform calculation
+                op = ops[self.operation]
+                res = op(self.dataCache, cdVal)
+
+                #Display the result to the calculater screen
+                self.outputLabel.setText(str(res))
+
+            except ValueError:
+                #If that fails, reset values and return an error
+                self.clearData()
+                self.outputLabel.setText("ERROR")
+
+            except ZeroDivisionError:
+                #If the user tries to divide by zero, reset values and return an error
+                self.clearData()
+                self.outputLabel.setText("ERROR")
 
 def main():
     app = QApplication(sys.argv)
